42c2e617ff27c8ab7eee025a8020ffd8fcd81217
venganza312 <gpfm312@gmail.com>
2021-11-12 14:18:30 +0900

    initialize msa-config-server

---
+++ src/test/java/com/venh/msa/config/server/VenhMsaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venh.msa.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenhMsaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
